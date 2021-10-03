92d8589e108655ba132d8bbdb05ae8993f060445
prabhakar <prabhakart.iam@gmail.com>
2021-10-03 12:01:22 +0400

    Project intial set up

---
+++ yhlproj/src/main/java/com/yhl/yhlproj/YhlprojApplication.java
@@ -0,0 +1,13 @@
+package com.yhl.yhlproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhlprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YhlprojApplication.class, args);
+	}
+
+}
---
+++ yhlproj/src/test/java/com/yhl/yhlproj/YhlprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhl.yhlproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YhlprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
